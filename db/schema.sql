8adf4d20e22852200ddc3fe57f1468f2f0db64ad
Irina Plaksina <irene.plaksina@gmail.com>
2020-08-03 11:31:36 -0400

    DB schema created

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+drop database if exists recipes_db;
+create database recipes_db;
+use recipes_db;
+
+create table recipes
+(
+    recipe_id     int auto_increment primary key,
+    recipe_name   varchar(50)    not null,
+    recipe_method varchar(10000) not null
+);
+
+create table measurement_units
+(
+    measurement_id          int auto_increment,
+    measurement_description varchar(10)
+);
+
+create table ingredients
+(
+    ingredient_id   int auto_increment,
+    ingredient_name varchar(30)
+);
+
+create table utencils
+(
+    utencil_id   int auto_increment,
+    utencil_name varchar(30)
+)
+
+create table recipe_ingredients
+(
+    recipe_id       int,
+    utencil_id      int,
+    ingredient_id   int,
+    measurement_qty int,
+    measurement_id  int,
+    constraint fk_recipe foreign key (recipe_id) references recipes (recipe_id),
+    constraint fk_utencil foreign key (utencil_id) references utencils (utencil_id),
+    constraint fk_ingredient foreign key (ingredient_id) references ingredients (ingredient_id),
+    constraint fk_measurement foreign key (measurement_id) references measurement_units (measurement_id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+insert into measurment_units (measurement_description)
+values ('cup'),
+       ('tsp'),
+       ('tbsp'),
+       ('gr'),
+       ('ml'),
+       ('pinch'),
+       ('handful');
+
+insert into ingredients (ingredient_name)
+values ('Bread'),
+       ('Milk'),
+       ('All-Purpose flour'),
+       ('Butter'),
+       ('Eggs'),
+       ('Ham'),
+       ('Bay Leaf'),
+       ('Nutmeg'),
+       ('Salt'),
+       ('Herbs'),
+       ('Classic Rolled Oatmeal'),
+       ('Water'),
+       ('Ground Cinnamon'),
+       ('Fruits');
+
+insert into recipes (recipe_name, recipe_method)
+values ('Crock Madame',
+        'Melt the butter in a pan on a low-high. Add flour, cook until it goldens. Add salt, nutmeg, and bay leaf, cook until thickened. Fry bread on a dry pan, both sides, until crispy. Spread half of the sauce over 1 slice of bread, put ham on top, add another slice of bread, spread the rest sauce, sprinkle with cheese, put under the grill for 7 mins. Fry an egg sunny side up. Take the sandwich from the oven, put the egg in the top, sprinkle with herbs.');
+
+insert into recipes (recipe_name, recipe_method)
+values ('Oatmeal',
+        'Mix everything together, Put in the microwave on max power for 3 mins or until it starts to boil. Watch to it doesn''t overflow. Leave in a microwave for 5 mins more.');
