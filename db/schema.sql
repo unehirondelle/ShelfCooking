abc87e27578fd4e298a57fc6266c483e550a941d
Irina Plaksina <irene.plaksina@gmail.com>
2020-08-03 12:26:47 -0400

    DB schema fixed - utensils are temporarily removed; Seeds added

--- db/schema.sql
+++ db/schema.sql
@@ -4,39 +4,38 @@ use recipes_db;
 
 create table recipes
 (
-    recipe_id         int auto_increment primary key,
-    recipe_name       varchar(50)    not null,
-    recipe_method     varchar(10000) not null,
-    recipe_time       varchar(10),
-    recipe_person_num int
+    id         int auto_increment primary key,
+    name       varchar(50)    not null,
+    method     varchar(10000) not null,
+    time       varchar(10),
+    person_num int
 );
 
 create table measurement_units
 (
-    measurement_id          int auto_increment primary key,
-    measurement_description varchar(10)
+    id   int auto_increment primary key,
+    name varchar(10)
 );
 
 create table ingredients
 (
-    ingredient_id   int auto_increment primary key,
-    ingredient_name varchar(30)
+    id   int auto_increment primary key,
+    name varchar(30)
 );
 
-create table utencils
+create table utensils
 (
-    utencil_id   int auto_increment primary key,
-    utencil_name varchar(30)
-)
+    id   int auto_increment primary key,
+    name varchar(30)
+);
 
 create table recipe_ingredients
 (
     recipe_id       int,
     ingredient_id   int,
-    measurement_qty int,
+    measurement_qty float,
     measurement_id  int,
-    constraint fk_recipe foreign key (recipe_id) references recipes (recipe_id),
-    constraint fk_utencil foreign key (utencil_id) references utencils (utencil_id),
-    constraint fk_ingredient foreign key (ingredient_id) references ingredients (ingredient_id),
-    constraint fk_measurement foreign key (measurement_id) references measurement_units (measurement_id)
+    constraint fk_recipe foreign key (recipe_id) references recipes (id),
+    constraint fk_ingredient foreign key (ingredient_id) references ingredients (id),
+    constraint fk_measurement foreign key (measurement_id) references measurement_units (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-insert into measurment_units (measurement_description)
+use recipes_db;
+
+insert into measurement_units (name)
 values ('cup'),
        ('tsp'),
        ('tbsp'),
@@ -6,9 +8,10 @@ values ('cup'),
        ('ml'),
        ('pinch'),
        ('handful'),
-       ('slice');
+       ('slice'),
+       ('ea');
 
-insert into ingredients (ingredient_name)
+insert into ingredients (name)
 values ('Bread'),
        ('Milk'),
        ('All-Purpose flour'),
@@ -24,20 +27,20 @@ values ('Bread'),
        ('Ground Cinnamon'),
        ('Fruits');
 
-insert into utencils (utencil_name)
+insert into utensils (name)
 values ('Knife'),
        ('Cutting Board'),
        ('Skillet'),
        ('Grill'),
        ('Microwave');
 
-insert into recipes (recipe_name, recipe_method, recipe_time, recipe_person_num)
+insert into recipes (name, method, time, person_num)
 values ('Crock Madame',
         'Melt the butter in a pan on a low-high. Add flour, cook until it goldens. Add salt, nutmeg, and bay leaf, cook until thickened. Fry bread on a dry pan, both sides, until crispy. Spread half of the sauce over 1 slice of bread, put ham on top, add another slice of bread, spread the rest sauce, sprinkle with cheese, put under the grill for 7 mins. Fry an egg sunny side up. Take the sandwich from the oven, put the egg in the top, sprinkle with herbs.',
         '20 min',
         '1');
 
-insert into recipes (recipe_name, recipe_method, recipe_time, recipe_person_num)
+insert into recipes (name, method, time, person_num)
 values ('Oatmeal',
         'Mix everything together, Put in the microwave on max power for 3 mins or until it starts to boil. Watch to it doesn''t overflow. Leave in a microwave for 5 mins more.',
         '8 min',
@@ -47,4 +50,40 @@ insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measu
 values (1, 1, 2, 8);
 
 insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
-values (1, 1, 2, 8)
+values (1, 2, 1, 1);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 3, 1, 3);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 4, 1, 3);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 5, 1, 9);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 6, 1, 8);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 7, 2, 9);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 8, 1, 2);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 9, 0.5, 2);
+
+insert into recipe_ingredients (recipe_id, ingredient_id, measurement_qty, measurement_id)
+values (1, 10, 1, 6);
+
+select r.name             as 'Recipe',
+       r.time             as 'Time',
+       r.person_num       as 'Persons',
+       i.name             as 'Ingredient',
+       ri.measurement_qty as 'Amount',
+       mu.name            as 'Unit of Measure',
+       r.method           as 'Method'
+from recipes r
+         join recipe_ingredients ri on r.id = ri.recipe_id
+         join ingredients i on i.id = ri.ingredient_id
+         left outer join measurement_units mu on mu.id = measurement_id;
